93425307a73bfb844a82c47bf3449d89a1be49ac
nexcodes <113250876+nexcodes@users.noreply.github.com>
2025-05-12 15:16:18 +0500

    idk

--- client/src/app/(main)/(root)/page.tsx
+++ client/src/app/(main)/(root)/page.tsx
@@ -1,10 +1,9 @@
 import { getUserProfile } from "@/data/get-user-profile";
 import { notFound } from "next/navigation";
 import ProfileContent from "./profile-content";
-import { retryPromise } from "@/func/retry";
 
 export default async function ProfilePage() {
-  const profile = await retryPromise(getUserProfile, 3, 50);
+  const profile = await getUserProfile();
 
   if (!profile) {
     return notFound();
