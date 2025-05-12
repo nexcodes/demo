1c93b8278c00d8e32bcb703aea24a5469875bda8
nexcodes <113250876+nexcodes@users.noreply.github.com>
2025-05-12 16:19:09 +0500

    idk

--- client/src/lib/supabase/server.ts
+++ client/src/lib/supabase/server.ts
@@ -3,10 +3,11 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { type SupabaseClient } from "@supabase/supabase-js";
 
 export const createServerSupabaseClient = (): SupabaseClient => {
+  // Call `cookies()` here, not at module level
+  const cookieStore = cookies();
+
   return createServerComponentClient(
-    {
-      cookies: () => cookies(),
-    },
+    { cookies: () => cookieStore },
     {
       supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY!,
       supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
